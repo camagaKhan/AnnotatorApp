41e48d6a756c71f2a825a06fc24fdc04de620d87
Liam Mulvaney <camagaKhan@gmail.com>
2024-05-22 19:20:40 +0200

    test

--- src/api/annotateRequests.js
+++ src/api/annotateRequests.js
@@ -1,14 +1,21 @@
 import axios from 'axios'
 
-let baseURL = 'https://cool-lines-fly.loca.lt/api/annotate'
+let baseURL = 'https://vast-swans-double.loca.lt/api/annotate'
+
+axios.create({
+    baseURL,
+    headers : {
+        'bypass-tunnel-reminder' : true
+    }
+})
 
 export const requests = {
     get: async (url) => {
-        const { data } = await axios.get(`${baseURL}${url}`)
+        const { data } = await axios.get(url)
         return data
     },
     post : async (url, obj) => {
-        return await axios.post(`${baseURL}${url}`, obj)
+        return await axios.post(url, obj)
     }
 }
 
