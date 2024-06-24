dccea785362bc43ab6446e47d90e07b5c277dc0b
Liam Mulvaney <camagaKhan@gmail.com>
2024-06-24 18:16:21 +0200

    do it again

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import { observer } from "mobx-react";
 import AppContainer from './components/AppContainer/AppContainer'
 import { ConfigProvider } from "antd";
 import { v4 as uuidv4 } from 'uuid'
-//import CommentWizard from "./components/wizard/wizard";
 
 
 function App() {
   return (
-    <ConfigProvider csp={{ nonce: uuidv4() }}>
+    <ConfigProvider csp={{ nonce: 'nonce-annotatorStuff' }}>
       <AppContainer />
     </ConfigProvider>    
   );
